1ad2d1cbe808f830d3b67491b7c6eca0e53efe5c
Pierre <ph_7@hotmail.be>
2012-04-22 04:09:56 +0300

    GPL to LGPL for better integration with other scripts

--- MySQLDump.class.php
+++ MySQLDump.class.php
@@ -26,7 +26,7 @@ namespace PH7\SQL\Backup;
  * @email       pierrehs@hotmail.com
  * @link        http://github.com/pH-7
  * @copyright   Copyright pH7 Script All Rights Reserved.
- * @license     GNU GPL 3 (www.gnu.org/licenses/gpl-3.0.html)
+ * @license     Distributed under the Lesser General Public License (LGPL) (http://www.gnu.org/copyleft/lesser.html)
  * @version     $Id: MySQLDump.class.php 1.2
  * @create 2012-02-13
  * @update 2012-04-21
