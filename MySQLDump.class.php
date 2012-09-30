5da45601f8393deca0743aff3c2d60c9ed967226
Pierre-Henry Soria <hizup.com@hotmail.com>
2012-09-30 13:34:16 +0200

    Changed the coding standards PSR-1/2 and PEAR

--- MySQLDump.class.php
+++ MySQLDump.class.php
@@ -4,7 +4,11 @@ namespace PH7\SQL\Backup;
 /**
  * @class MySQLDump Class
  * @access public
+<<<<<<< HEAD
+ *
+=======
  * @desc
+>>>>>>> d52d54ddb0f0ed55572197d6316642f6eec89dd0
  * Backs up a database, creating a file for each day of the week,
  * using the mysqldump utility.
  * Can compress backup file with gzip of bzip2.
@@ -22,8 +26,12 @@ namespace PH7\SQL\Backup;
  *
  * @use Please run your php script using this class (~/MySQLDump.class.php) with a cron (e.g. cPanel, Plesk, ...) and execute this script at a time periodic.
  *
+<<<<<<< HEAD
+ * @author      Pierre-Henry Soria <pierrehs@hotmail.com>
+=======
  * @author      Pierre-Henry Soria
  * @email       pierrehs@hotmail.com
+>>>>>>> d52d54ddb0f0ed55572197d6316642f6eec89dd0
  * @link        http://github.com/pH-7
  * @copyright   Pierre-Henry Soria, All Rights Reserved.
  * @license     Lesser General Public License (LGPL) (http://www.gnu.org/copyleft/lesser.html)
@@ -31,6 +39,63 @@ namespace PH7\SQL\Backup;
  * @create      2012-02-13
  * @update      2012-05-28
  */
+<<<<<<< HEAD
+class MySQL
+{
+
+    /**
+     * The backup command to execute.
+     *
+     * @access private
+     * @var string $_sCmd
+     */
+    private $_sCmd;
+
+    /**
+     * The extension allowed.
+     *
+     * @access private
+     * @var array $_aZipExt
+     */
+    private $_aZipExt = array('gz' => 'gzip', 'bz2' => 'bzip2');
+
+    /**
+     * MySQLDump constructor.
+     *
+     * @access public
+     * @param string $sDbHost MySQL Host Name
+     * @param string $sDbUser MySQL User Name
+     * @param string $sDbPass MySQL User Password
+     * @param string $sDbName Database to select
+     * @param string $sDest The path to folder where the file will be stored backup
+     * @param string $sZip Zip type; gz - gzip [default], bz2 - bzip
+     * @return void
+     */
+    public function __construct($sDbHost, $sDbUser, $sDbPass, $sDbName, $sDest, $sZip = 'gz')
+    {
+        $bZip = (array_key_exists($sZip, $this->_aZipExt)) ? true : false;
+        $sExt = ($bZip) ? '.' . $sZip : '';
+        $sFileName = 'Periodic-database-update.' . date('Y-m-d') . '.sql' . $sExt;
+        $sOptions = ($bZip) ? ' | ' . $this->_aZipExt[$sZip] : '';
+
+        $this->_sCmd = 'mysqldump -h' . $sDbHost . ' -u' . $sDbUser . ' -p' . $sDbPass . ' ' . $sDbName . $sOptions . ' > ' . $sDest . $sFileName;
+    }
+
+    /**
+     * Runs the constructed command.
+     *
+     * @access public
+     * @return void
+     */
+    public function backup()
+    {
+        $sError = '';
+        system($this->_sCmd, $sError);
+        if ($sError) {
+            trigger_error('Backup failed: Command = ' . $this->_sCmd . ' Error = ' . $sError);
+        }
+    }
+=======
 
 class MySQL {
 
@@ -83,6 +148,6 @@ class MySQL {
       trigger_error('Backup failed: Command = ' . $this->sCmd . ' Error = ' . $sError);
     }
   }
+>>>>>>> d52d54ddb0f0ed55572197d6316642f6eec89dd0
 
 }
-
